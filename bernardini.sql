72f04e55bd3ef020802cdabf4c631961482c949f
LucaBernardinii <luca.bernardini@studenti.isissgobetti.it>
2026-04-14 09:47:34 +0200

    query

--- bernardini.sql
+++ bernardini.sql
@@ -1,21 +1,23 @@
 CREATE TABLE Prodotto (
     id INT PRIMARY KEY,
     nome TEXT NOT NULL,
-    prezzo REAL,
-)
+    prezzo REAL
+);
 
 CREATE TABLE Ordine (
     id INT PRIMARY KEY,
     data DATE,
-    cliente_nome TEXT NOT NULL,
-)
+    cliente_nome TEXT NOT NULL
+);
 
 CREATE TABLE OrdineProdotto (
     id INT PRIMARY KEY,
-    ordine_id INT FOREIGN KEY,
-    prodotto_id INT FOREIGN KEY,
+    ordine_id INT NOT NULL,
+    prodotto_id INT NOT NULL,
     quantita INT,
-)
+    FOREIGN KEY (ordine_id) REFERENCES Ordine(id),
+    FOREIGN KEY (prodotto_id) REFERENCES Prodotto(id)
+);
 
 INSERT INTO Prodotto (id, nome, prezzo) VALUES
 (1, 'Notebook', 12.5),
@@ -36,3 +38,22 @@ INSERT INTO OrdineProdotto (id, ordine_id, prodotto_id, quantita) VALUES
 (4, 3, 4, 2),
 (5, 3, 2, 3);
 
+-- Query 1
+-- Seleziona tutte le informazioni dei prodotti che costano meno di 10
+-- dalla tabella Prodotto
+SELECT *
+FROM Prodotto
+WHERE prezzo < 10;
+
+-- Query 2
+
+-- Query 3
+-- Conta quante volte il nome di un cliente è contenuto nella tabella Ordine
+SELECT cliente_nome, COUNT(*) AS numero_ordini
+FROM Ordine
+GROUP BY cliente_nome;
+
+-- Query 4
+-- Fa la media dei prezzi nella tabella Prodotto
+SELECT AVG(prezzo) AS prezzo_medio
+FROM Prodotto;
